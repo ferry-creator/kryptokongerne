323da2d69f95245b6671226049016581512f22f6
ferry-creator <ferryisworking@gmail.com>
2022-06-17 12:58:34 +0200

    Add MQTT broker struct

--- project3/src/Kode_MCU_top/Kode_MCU_top.ino
+++ project3/src/Kode_MCU_top/Kode_MCU_top.ino
@@ -1,4 +1,3 @@
-
 //////////// Initiering ///////////
 
 //inkluderer nødvendige bibloteker
@@ -8,6 +7,70 @@
 #include <ArduinoJson.h>
 #include <ESP32Servo.h>
 
+#define hivemq_host "d442f1008e514e098cb3126d115cb356.s1.eu.hivemq.cloud"
+#define hivemq_user "krypto"
+#define hivemq_pass "hivemq1234"
+#define hivemq_port 8883
+
+#define cloudmqtt_host "hairdresser.cloudmqtt.com"
+#define cloudmqtt_user "jnyhgkwt"
+#define cloudmqtt_pass "Zwu25yVhkqBN"
+#define cloudmqtt_port 15687
+
+#define maqia_host "maqiatto.com"
+#define maqia_user "s203773@student.dtu.dk"
+#define maqia_pass "alex123"
+#define maqia_port 1883
+
+struct mqttBroker {
+  char *host;
+  char *user;
+  char *pass;
+  int   port;
+};
+
+const mqttBroker cloudmqtt = { cloudmqtt_host, cloudmqtt_user, cloudmqtt_pass, cloudmqtt_port };
+const mqttBroker hivemq    = { hivemq_host, hivemq_user, hivemq_pass, hivemq_port };
+const mqttBroker maqiatto  = { maqia_host, maqia_user, maqia_pass, maqia_port };
+
+// Change this to change broker
+const mqttBroker MQTT = maqiatto;
+
+//////////// HiveMQ TSL ///////////
+const char* root_ca= \
+     "-----BEGIN CERTIFICATE-----\n" \
+     "MIIFFjCCAv6gAwIBAgIRAJErCErPDBinU/bWLiWnX1owDQYJKoZIhvcNAQELBQAw\n" \
+     "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
+     "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMjAwOTA0MDAwMDAw\n" \
+     "WhcNMjUwOTE1MTYwMDAwWjAyMQswCQYDVQQGEwJVUzEWMBQGA1UEChMNTGV0J3Mg\n" \
+     "RW5jcnlwdDELMAkGA1UEAxMCUjMwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK\n" \
+     "AoIBAQC7AhUozPaglNMPEuyNVZLD+ILxmaZ6QoinXSaqtSu5xUyxr45r+XXIo9cP\n" \
+     "R5QUVTVXjJ6oojkZ9YI8QqlObvU7wy7bjcCwXPNZOOftz2nwWgsbvsCUJCWH+jdx\n" \
+     "sxPnHKzhm+/b5DtFUkWWqcFTzjTIUu61ru2P3mBw4qVUq7ZtDpelQDRrK9O8Zutm\n" \
+     "NHz6a4uPVymZ+DAXXbpyb/uBxa3Shlg9F8fnCbvxK/eG3MHacV3URuPMrSXBiLxg\n" \
+     "Z3Vms/EY96Jc5lP/Ooi2R6X/ExjqmAl3P51T+c8B5fWmcBcUr2Ok/5mzk53cU6cG\n" \
+     "/kiFHaFpriV1uxPMUgP17VGhi9sVAgMBAAGjggEIMIIBBDAOBgNVHQ8BAf8EBAMC\n" \
+     "AYYwHQYDVR0lBBYwFAYIKwYBBQUHAwIGCCsGAQUFBwMBMBIGA1UdEwEB/wQIMAYB\n" \
+     "Af8CAQAwHQYDVR0OBBYEFBQusxe3WFbLrlAJQOYfr52LFMLGMB8GA1UdIwQYMBaA\n" \
+     "FHm0WeZ7tuXkAXOACIjIGlj26ZtuMDIGCCsGAQUFBwEBBCYwJDAiBggrBgEFBQcw\n" \
+     "AoYWaHR0cDovL3gxLmkubGVuY3Iub3JnLzAnBgNVHR8EIDAeMBygGqAYhhZodHRw\n" \
+     "Oi8veDEuYy5sZW5jci5vcmcvMCIGA1UdIAQbMBkwCAYGZ4EMAQIBMA0GCysGAQQB\n" \
+     "gt8TAQEBMA0GCSqGSIb3DQEBCwUAA4ICAQCFyk5HPqP3hUSFvNVneLKYY611TR6W\n" \
+     "PTNlclQtgaDqw+34IL9fzLdwALduO/ZelN7kIJ+m74uyA+eitRY8kc607TkC53wl\n" \
+     "ikfmZW4/RvTZ8M6UK+5UzhK8jCdLuMGYL6KvzXGRSgi3yLgjewQtCPkIVz6D2QQz\n" \
+     "CkcheAmCJ8MqyJu5zlzyZMjAvnnAT45tRAxekrsu94sQ4egdRCnbWSDtY7kh+BIm\n" \
+     "lJNXoB1lBMEKIq4QDUOXoRgffuDghje1WrG9ML+Hbisq/yFOGwXD9RiX8F6sw6W4\n" \
+     "avAuvDszue5L3sz85K+EC4Y/wFVDNvZo4TYXao6Z0f+lQKc0t8DQYzk1OXVu8rp2\n" \
+     "yJMC6alLbBfODALZvYH7n7do1AZls4I9d1P4jnkDrQoxB3UqQ9hVl3LEKQ73xF1O\n" \
+     "yK5GhDDX8oVfGKF5u+decIsH4YaTw7mP3GFxJSqv3+0lUFJoi5Lc5da149p90Ids\n" \
+     "hCExroL1+7mryIkXPeFM5TgO9r0rvZaBFOvV2z0gp35Z0+L4WPlbuEjN/lxPFin+\n" \
+     "HlUjr8gRsI3qfJOQFy/9rKIJR0Y/8Omwt/8oTWgy1mdeHmmjk7j1nYsvC9JSQ6Zv\n" \
+     "MldlTTKB3zhThV1+XWYp6rjd5JW1zbVWEkLNxE7GJThEUG3szgBVGP7pSWTUTsqX\n" \
+     "nLRbwHOoq7hHwg==\n" \
+     "-----END CERTIFICATE-----\n";
+
+//////////////////////////////////
+
 //Setting servo name
 Servo servo;
 
@@ -15,17 +78,9 @@ Servo servo;
 int motorPin1 = 12;
 int motorPin2 = 14;
 
-
 // Definerer id og password til netværksforbindelse som NodeMCU anvender
 const char* ssid = "OnePlus 7"; //Indsæt navnet på jeres netværk her
 const char* password = "55012bceb395"; //Indsæt password her
-
-// Definerer information til mqtt serveren
-const char *mqtt_server = "hairdresser.cloudmqtt.com"; //navn på mqtt-server. Find navnet på cloudmqtt-hjemmesiden
-const int mqtt_port = 15687; // Definerer porten
-const char *mqtt_user = "jnyhgkwt"; // Definerer mqtt-brugeren
-const char *mqtt_pass = "Zwu25yVhkqBN"; // Definerer koden til mqtt-brugeren
-//
 
 String payload; // Definerer variablen 'payload' i det globale scope (payload er navnet på besked-variablen)
 
@@ -54,7 +109,7 @@ void callback(char* topic, byte* Payload, unsigned int length);
 WiFiClient espClient; // Initialiserer wifi bibloteket ESP8266Wifi, som er inkluderet under "nødvendige bibloteker"
 
 // Opretter forbindelse til mqtt klienten:
-PubSubClient client(mqtt_server, mqtt_port, callback, espClient); // Initialiserer bibloteket for at kunne modtage og sende beskeder til mqtt. Den henter fra definerede mqtt server og port. Den henter fra topic og beskeden payload
+PubSubClient client(MQQT.host, MQQT.port, callback, espClient); // Initialiserer bibloteket for at kunne modtage og sende beskeder til mqtt. Den henter fra definerede mqtt server og port. Den henter fra topic og beskeden payload
 
 /////// FUNKTIONSOPSÆTNING SLUT /////////
 
@@ -204,7 +259,7 @@ void reconnect() {
   while (!client.connected()) {
     Serial.print("Forsøger at oprette MQTT forbindelse...");
 
-    if (client.connect("krypto", mqtt_user, mqtt_pass)) { // Forbinder til klient med mqtt bruger og password
+    if (client.connect("krypto", MQTT.user, MQTT.pass)) { // Forbinder til klient med mqtt bruger og password
       Serial.println("connected");
       // Derudover subsribes til topic "Test" hvor NodeMCU modtager payload beskeder fra
       client.subscribe("dispenser");
